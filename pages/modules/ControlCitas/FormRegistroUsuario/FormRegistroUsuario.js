26ada20beffc38d88022ca68b46e7adb5d494628
jhon.montoya <jhon.montoya@compra123.com>
2021-04-28 19:48:45 -0500

    se valida al momento de crear una cita

--- pages/modules/ControlCitas/FormRegistroUsuario/FormRegistroUsuario.js
+++ pages/modules/ControlCitas/FormRegistroUsuario/FormRegistroUsuario.js
@@ -38,6 +38,10 @@ export default function FormRegistroUsuario({
     setDisabledButton(false);
     if (data.data === "Paciente agregado correctamente") {
       swal("Exelente", "Paciente creado", "success");
+<<<<<<< HEAD
+=======
+
+>>>>>>> se valida al momento de crear una cita
       setOpen(false);
       getAllData();
       if (!accion) {
@@ -53,6 +57,7 @@ export default function FormRegistroUsuario({
   };
 
   const onSubmit = (data) => {
+<<<<<<< HEAD
 
     if (accion === "actualizar") {
       setDisabledButton(true);
@@ -61,6 +66,15 @@ export default function FormRegistroUsuario({
       setDisabledButton(true);
       let nombre = document.getElementById("nombre").value;
       //let apellidos = document.getElementById("apellidos").value;
+=======
+    if (accion === "actualizar") {
+      setDisabledButton(true);
+      put(urlPut, data, setDataResponse);
+    } else if (accion === "crear") {
+      setDisabledButton(true);
+      let nombre = document.getElementById("nombre").value;
+      let apellidos = document.getElementById("apellidos").value;
+>>>>>>> se valida al momento de crear una cita
       let cedula = document.getElementById("cedula").value;
       let fechaNacimiento = document.getElementById("fecha_nacimiento").value;
       let celular = document.getElementById("celular").value;
@@ -69,7 +83,11 @@ export default function FormRegistroUsuario({
       let ocupacion = document.getElementById("ocupacion").value;
       if (
         nombre === "" ||
+<<<<<<< HEAD
         // apellidos === "" ||
+=======
+        apellidos === "" ||
+>>>>>>> se valida al momento de crear una cita
         cedula === "" ||
         fechaNacimiento === "" ||
         celular === "" ||
@@ -81,16 +99,24 @@ export default function FormRegistroUsuario({
           button: {
             text: "De acuerdo!",
           },
+<<<<<<< HEAD
           
         });
         setDisabledButton(false);
+=======
+        });
+>>>>>>> se valida al momento de crear una cita
       } else {
         post(url, Object.assign(data, fechaRegistro), setDataResponse);
         setCedulaPaciente(data.cedula);
       }
     }
   };
+<<<<<<< HEAD
 console.log(dataUser);
+=======
+
+>>>>>>> se valida al momento de crear una cita
   return (
     <form
       className="flex flex-col w-9/12 mx-16 md:mx-0"
@@ -101,17 +127,28 @@ console.log(dataUser);
         name="nombre"
         id="nombre"
         defaultValue={dataUser?.nombre}
+<<<<<<< HEAD
         placeholder="Nombre completo"
         ref={register}
       />
       {/* <input
+=======
+        placeholder="Nombre"
+        ref={register}
+      />
+      <input
+>>>>>>> se valida al momento de crear una cita
         className="border-2 border-gray-400 rounded-md m-3 text-xl"
         name="apellidos"
         id="apellidos"
         defaultValue={dataUser?.apellidos}
         placeholder="Apellidos"
         ref={register}
+<<<<<<< HEAD
       /> */}
+=======
+      />
+>>>>>>> se valida al momento de crear una cita
       <input
         className="border-2 border-gray-400 rounded-md m-3 text-xl"
         name="cedula"
@@ -181,8 +218,13 @@ console.log(dataUser);
       />
 
       <input
+<<<<<<< HEAD
         disabled={disabledButton}
         className={disabledButton ? "bg-gray-500 py-1 px-5 rounded-md text-white font-semibold" : "bg-blue-700 py-1 px-5 rounded-md text-white font-semibold"}
+=======
+        // disabled={disabledButton}
+        className="bg-blue-700 py-1 px-5 rounded-md text-white font-semibold"
+>>>>>>> se valida al momento de crear una cita
         type="submit"
       />
     </form>
