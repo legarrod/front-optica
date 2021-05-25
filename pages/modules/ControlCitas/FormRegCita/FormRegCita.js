3a502c3056173b7964ffd7e0f6630275599bedad
legarrod <luis.garcia@atlanticsoft.us>
2021-05-24 20:40:17 -0500

    se amplio las horas para asignar citas

--- pages/modules/ControlCitas/FormRegCita/FormRegCita.js
+++ pages/modules/ControlCitas/FormRegCita/FormRegCita.js
@@ -1,152 +1,200 @@
-import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import swal from "sweetalert";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import swal from 'sweetalert';
+import axios from 'axios';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
-import getDate from "../../../utils/utils";
+import getDate from '../../../utils/utils';
 
-export default function FormRegCita({ setOpen, cedulaPaciente,  getDataEvent }) {
-  let hoy = new Date();
-  const url = `${process.env.API_REGISTRAR_NUEVA_CITA}`;
-  const urlConsultarPaciente = `${process.env.API_OBTENER_TODOS_LOS_PACIENTES}`;
-  const [fecha, setFecha] = useState('');
-  const [data, setData] = useState();
-  const [idPaciente, setIdPaciente] = useState();
-  const [horaCita, setHoraCita] = useState('');
-  const hora = ['09:00 am', '09:15 am', '09:30 am', '09:45 am', '10:00 am', '10:15 am', '10:30 am', '10:45 am', '11:00 am', '11:15 am', '11:30 am', '11:45 am', '12:00 pm', '12:15 pm', '12:30 pm', '12:45 pm', '01:00 pm', '01:15 pm', '01:30 pm', '01:45 pm', '02:00 pm', '02:15 pm', '02:30 pm', '02:45 pm', '03:00 pm', '03:15 pm', '03:30 pm', '03:45 pm', '04:00 pm', '04:15 pm', '04:30 pm', '04:45 pm', '05:00 pm', '05:15 pm', '05:30 pm', '05:45 pm']
-  const [disabledButton, setDisabledButton] = useState(false)
-  const { register, handleSubmit } = useForm();
+export default function FormRegCita({ setOpen, cedulaPaciente, getDataEvent }) {
+	let hoy = new Date();
+	const url = `${process.env.API_REGISTRAR_NUEVA_CITA}`;
+	const urlConsultarPaciente = `${process.env.API_OBTENER_TODOS_LOS_PACIENTES}`;
+	const [fecha, setFecha] = useState('');
+	const [data, setData] = useState();
+	const [idPaciente, setIdPaciente] = useState();
+	const [horaCita, setHoraCita] = useState('');
+	const hora = [
+		'09:00 am',
+		'09:15 am',
+		'09:30 am',
+		'09:45 am',
+		'10:00 am',
+		'10:15 am',
+		'10:30 am',
+		'10:45 am',
+		'11:00 am',
+		'11:15 am',
+		'11:30 am',
+		'11:45 am',
+		'12:00 pm',
+		'12:15 pm',
+		'12:30 pm',
+		'12:45 pm',
+		'01:00 pm',
+		'01:15 pm',
+		'01:30 pm',
+		'01:45 pm',
+		'02:00 pm',
+		'02:15 pm',
+		'02:30 pm',
+		'02:45 pm',
+		'03:00 pm',
+		'03:15 pm',
+		'03:30 pm',
+		'03:45 pm',
+		'04:00 pm',
+		'04:15 pm',
+		'04:30 pm',
+		'04:45 pm',
+		'05:00 pm',
+		'05:15 pm',
+		'05:30 pm',
+		'05:45 pm',
+		'06:00 pm',
+		'06:15 pm',
+		'06:30 pm',
+		'06:45 pm',
+		'07:00 pm',
+		'07:15 pm',
+		'07:30 pm',
+		'07:45 pm',
+		'08:00 pm',
+	];
+	const [disabledButton, setDisabledButton] = useState(false);
+	const { register, handleSubmit } = useForm();
 
+	const setDataResponse = (data) => {
+		if (data.data.data === true) {
+			swal('Excelente', 'Se ha registrado una nueva cita', 'success');
+			setOpen(false);
+			getDataEvent();
+			setDisabledButton(false);
+		}
+	};
 
-  const setDataResponse =(data)=>{
- 
-    if (data.data.data === true) {
-      swal("Excelente", "Se ha registrado una nueva cita", "success");
-      setOpen(false);
-      getDataEvent();
-      setDisabledButton(false)
-    }
-  }
+	const postCita = async (url, formData = null) => {
+		try {
+			const data = await axios.post(url, formData);
 
-  const postCita = async (url, formData = null) => {
-    try {
-      const data = await axios.post(url, formData);
-      
-        setDataResponse(data);
-    
-    } catch (error) {
-      console.log(error);
-    }
-    //return axios.post(url, formData, config);
-  };
+			setDataResponse(data);
+		} catch (error) {
+			console.log(error);
+		}
+		//return axios.post(url, formData, config);
+	};
 
-  const onSubmit = (data) => {
-    let newData = {
-          fk_id_paciente: parseInt(idPaciente),
-          nombre_doctor: 1,
-          fk_id_sede: 1,
-          fk_id_estado: "",
-          fecha_creacion: getDate(hoy),
-          fecha_cita: fecha,
-          hora: horaCita,
-          lejos_add_derecho: "", 
-          lejos_add_izquierdo: "", 
-          cerca_add_derecho: "", 
-          cerca_add_izquierdo: "",
-          cerca_av_derecho: "",
-          cerca_av_izquierdo: "",
-          cerca_cilindro_derecho: "",
-          cerca_cilindro_izquierdo: "",
-          cerca_eje_derecho: "",
-          cerca_eje_izquierdo: "",
-          cerca_esferico_derecho: "",
-          cerca_esferico_izquierdo: "",
-          lejos_av_derecho: "",
-          lejos_av_izquierdo: "",
-          lejos_cilindro_derecho: "",
-          lejos_cilindro_izquierdo: "",
-          lejos_eje_derecho: "",
-          lejos_eje_izquierdo: "",
-          lejos_esferico_derecho: "",
-          lejos_esferico_izquierdo: "",
-          valor_cita: "",
-          observaciones: ""
-      }
-      if (
-        fecha === "" ||
-        horaCita === "" 
-      ) {
-        swal({
-          text: "Por favor complete todos los campos",
-          button: {
-            text: "De acuerdo!",
-          },
-          
-        });
-        setDisabledButton(false);
-      } else {
-        setDisabledButton(true)
-          postCita(url, newData, setDataResponse)
-      }
-  };
+	const onSubmit = (data) => {
+		let newData = {
+			fk_id_paciente: parseInt(idPaciente),
+			nombre_doctor: 1,
+			fk_id_sede: 1,
+			fk_id_estado: '',
+			fecha_creacion: getDate(hoy),
+			fecha_cita: fecha,
+			hora: horaCita,
+			lejos_add_derecho: '',
+			lejos_add_izquierdo: '',
+			cerca_add_derecho: '',
+			cerca_add_izquierdo: '',
+			cerca_av_derecho: '',
+			cerca_av_izquierdo: '',
+			cerca_cilindro_derecho: '',
+			cerca_cilindro_izquierdo: '',
+			cerca_eje_derecho: '',
+			cerca_eje_izquierdo: '',
+			cerca_esferico_derecho: '',
+			cerca_esferico_izquierdo: '',
+			lejos_av_derecho: '',
+			lejos_av_izquierdo: '',
+			lejos_cilindro_derecho: '',
+			lejos_cilindro_izquierdo: '',
+			lejos_eje_derecho: '',
+			lejos_eje_izquierdo: '',
+			lejos_esferico_derecho: '',
+			lejos_esferico_izquierdo: '',
+			valor_cita: '',
+			observaciones: '',
+		};
+		if (fecha === '' || horaCita === '') {
+			swal({
+				text: 'Por favor complete todos los campos',
+				button: {
+					text: 'De acuerdo!',
+				},
+			});
+			setDisabledButton(false);
+		} else {
+			setDisabledButton(true);
+			postCita(url, newData, setDataResponse);
+		}
+	};
 
-  const handlerSlectHora = (e)=>{
-    setHoraCita(e.target.value);
-  }
+	const handlerSlectHora = (e) => {
+		setHoraCita(e.target.value);
+	};
 
-  const obtenerPaciente = async (urlConsultarPaciente, cedulaPaciente, setData) => {
-    let urlN = `${urlConsultarPaciente}/${cedulaPaciente}`
-    try {
-      const { data } = await axios.get(urlN);
-     
-      if (data) {
-        setData(data);
-        setIdPaciente(data.id)
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+	const obtenerPaciente = async (
+		urlConsultarPaciente,
+		cedulaPaciente,
+		setData
+	) => {
+		let urlN = `${urlConsultarPaciente}/${cedulaPaciente}`;
+		try {
+			const { data } = await axios.get(urlN);
 
-  useEffect(() => {
-    obtenerPaciente(urlConsultarPaciente, cedulaPaciente, setData)
-  }, []);
+			if (data) {
+				setData(data);
+				setIdPaciente(data.id);
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  return (
-    <form
-      className="flex flex-wrap justify-center"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-wrap justify-center">
-        <input
-          className="border-2 border-gray-400 rounded-md m-3 text-xl"
-          name="nombre"
-          placeholder="Nombre"
-          defaultValue={data?.nombre}
-          ref={register}
-        />
-        <div className="flex flex-wrap mb-2 items-center">
-        <p className="text-xl mr-3">Dia de la cita:</p>
-        <DayPickerInput style={{border: '1px gray solid'}} onDayChange={day => setFecha(getDate(day))} />
-        </div>
-        <div>
-        <select className="border-2 p-2 bg-white rounded-md text-xl my-3 sm:m-3 w-80" name="hora" ref={register} onClick={(e) => handlerSlectHora(e)}>
-				{
-					hora?.map(item=>(
-						<option value={item}>{item}</option>
-					))
-				}
-			</select>
-        </div>
-        
-      </div>
-      
-      <input
-      disabled={disabledButton}
-        className="bg-blue-700 py-1 px-5 rounded-md text-white font-semibold"
-        type="submit"
-      />
-    </form>
-  );
+	useEffect(() => {
+		obtenerPaciente(urlConsultarPaciente, cedulaPaciente, setData);
+	}, []);
+
+	return (
+		<form
+			className="flex flex-wrap justify-center"
+			onSubmit={handleSubmit(onSubmit)}
+		>
+			<div className="flex flex-wrap justify-center">
+				<input
+					className="border-2 border-gray-400 rounded-md m-3 text-xl"
+					name="nombre"
+					placeholder="Nombre"
+					defaultValue={data?.nombre}
+					ref={register}
+				/>
+				<div className="flex flex-wrap mb-2 items-center">
+					<p className="text-xl mr-3">Dia de la cita:</p>
+					<DayPickerInput
+						style={{ border: '1px gray solid' }}
+						onDayChange={(day) => setFecha(getDate(day))}
+					/>
+				</div>
+				<div>
+					<select
+						className="border-2 p-2 bg-white rounded-md text-xl my-3 sm:m-3 w-80"
+						name="hora"
+						ref={register}
+						onClick={(e) => handlerSlectHora(e)}
+					>
+						{hora?.map((item) => (
+							<option value={item}>{item}</option>
+						))}
+					</select>
+				</div>
+			</div>
+
+			<input
+				disabled={disabledButton}
+				className="bg-blue-700 py-1 px-5 rounded-md text-white font-semibold"
+				type="submit"
+			/>
+		</form>
+	);
 }
--- pages/modules/ControlCitas/TablaCitas/TablaCitas.js
+++ pages/modules/ControlCitas/TablaCitas/TablaCitas.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -9,72 +9,114 @@ import { getRowEl } from '@material-ui/data-grid';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
 import Link from 'next/link';
-import Button from "@material-ui/core/Button";
-import getDate from "../../../utils/utils"
-import { put} from '../../../api/AsyncHttpRequest'
-import swal from "sweetalert";
+import Button from '@material-ui/core/Button';
+import getDate from '../../../utils/utils';
+import { put } from '../../../api/AsyncHttpRequest';
+import swal from 'sweetalert';
 
 const StyledTableCell = withStyles((theme) => ({
 	head: {
-	  backgroundColor: theme.palette.common.black,
-	  color: theme.palette.common.white,
+		backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white,
 	},
 	body: {
-	  fontSize: 14,
+		fontSize: 14,
 	},
-  }))(TableCell);
+}))(TableCell);
 
-  const StyledTableRow = withStyles((theme) => ({
+const StyledTableRow = withStyles((theme) => ({
 	root: {
-	  '&:nth-of-type(odd)': {
-		backgroundColor: theme.palette.action.hover,
-	  },
+		'&:nth-of-type(odd)': {
+			backgroundColor: theme.palette.action.hover,
+		},
 	},
-  }))(TableRow);
+}))(TableRow);
 
 const useStyles = makeStyles((theme) => ({
-modal: {
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-},
-paper: {
-	backgroundColor: theme.palette.background.paper,
-	border: "2px solid #000",
-	boxShadow: theme.shadows[5],
-	padding: theme.spacing(2, 4, 3),
-},
+	modal: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	paper: {
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
 }));
 
-
-export default function TablaCitas({data, horasPorFecha, getHorasFecha}) {
-	const hora = ['09:00 am', '09:15 am', '09:30 am', '09:45 am', '10:00 am', '10:15 am', '10:30 am', '10:45 am', '11:00 am', '11:15 am', '11:30 am', '11:45 am', '12:00 pm', '12:15 pm', '12:30 pm', '12:45 pm', '01:00 pm', '01:15 pm', '01:30 pm', '01:45 pm', '02:00 pm', '02:15 pm', '02:30 pm', '02:45 pm', '03:00 pm', '03:15 pm', '03:30 pm', '03:45 pm', '04:00 pm', '04:15 pm', '04:30 pm', '04:45 pm', '05:00 pm', '05:15 pm', '05:30 pm', '05:45 pm']
-  	const [horasFiltradas, setHorasFiltradas] = useState([])
+export default function TablaCitas({ data, horasPorFecha, getHorasFecha }) {
+	const hora = [
+		'09:00 am',
+		'09:15 am',
+		'09:30 am',
+		'09:45 am',
+		'10:00 am',
+		'10:15 am',
+		'10:30 am',
+		'10:45 am',
+		'11:00 am',
+		'11:15 am',
+		'11:30 am',
+		'11:45 am',
+		'12:00 pm',
+		'12:15 pm',
+		'12:30 pm',
+		'12:45 pm',
+		'01:00 pm',
+		'01:15 pm',
+		'01:30 pm',
+		'01:45 pm',
+		'02:00 pm',
+		'02:15 pm',
+		'02:30 pm',
+		'02:45 pm',
+		'03:00 pm',
+		'03:15 pm',
+		'03:30 pm',
+		'03:45 pm',
+		'04:00 pm',
+		'04:15 pm',
+		'04:30 pm',
+		'04:45 pm',
+		'05:00 pm',
+		'05:15 pm',
+		'05:30 pm',
+		'05:45 pm',
+		'06:00 pm',
+		'06:15 pm',
+		'06:30 pm',
+		'06:45 pm',
+		'07:00 pm',
+		'07:15 pm',
+		'07:30 pm',
+		'07:45 pm',
+		'08:00 pm',
+	];
+	const [horasFiltradas, setHorasFiltradas] = useState([]);
 	let hoy = new Date();
 	const classes = useStyles();
-	const [nuevasHorasLimpias, setNuevasHorasLimpias] = useState([])
+	const [nuevasHorasLimpias, setNuevasHorasLimpias] = useState([]);
 	const url = `${process.env.API_ACTUALIZAR_HORA_CITA}/`;
-	
-const callbackResponse = (response)=>{
-	if (response?.data?.data) {
-		swal("Excelente", "Hora actualizada con exito!", "success")
-	  }
 
-}
+	const callbackResponse = (response) => {
+		if (response?.data?.data) {
+			swal('Excelente', 'Hora actualizada con exito!', 'success');
+		}
+	};
 
-	const handlerSaveHour = (data)=>{
+	const handlerSaveHour = (data) => {
 		let index = data.e.target.selectedIndex;
-		let newData = {hora: data.e.target.options[index].text}
+		let newData = { hora: data.e.target.options[index].text };
 
 		let defaultInfo = {
-				id_cita_paciente: parseInt(data?.row?.id_cita_paciente),
-				hora: newData.hora,
-			
-			}
+			id_cita_paciente: parseInt(data?.row?.id_cita_paciente),
+			hora: newData.hora,
+		};
 		//console.log(data.row);
-		put(url, defaultInfo, callbackResponse)
-		
-	}
+		put(url, defaultInfo, callbackResponse);
+	};
 
 	// const limpiarHoras = (horasPorFecha)=>{
 	// 	// horasPorFecha.forEach(item => {
@@ -87,54 +129,67 @@ const callbackResponse = (response)=>{
 
 	// 	limpiarHoras(horasPorFecha)
 	// 	//let nuevo = hora.filter((item)=> console.log(item ) )
-		
+
 	// }
 	// useEffect(() => {
 	// 	nuevasHorasFiltradas(horasPorFecha)
 	// }, [horasPorFecha])
 	// console.log(nuevasHorasLimpias);
 
-
-
 	return (
-	<div className='mx-2 sm:mx-10 mt-3'>	
-		<TableContainer className="h-96" component={Paper}>
-			<Table className={classes.table} size="small" aria-label="a dense table">
-				<TableHead>
-				<TableRow>
-					<StyledTableCell align="left">Nombre</StyledTableCell>
-					<StyledTableCell align="center">Fecha</StyledTableCell>
-					<StyledTableCell align="center">Hora</StyledTableCell>
-					<StyledTableCell align="center">Accion</StyledTableCell>
-				</TableRow>
-				</TableHead>
-				<TableBody>
-				{data && data?.map((row, index) => (
-					<StyledTableRow key={index}>
-						<StyledTableCell align="left"><p className=''> {`${row.nombre} ${row.apellidos}`}</p></StyledTableCell>
-						<StyledTableCell align="center">{row.fecha_cita}</StyledTableCell>
-						{/* <StyledTableCell align="center">{row.hora !== '' ? row.hora : 'Sin hora'}</StyledTableCell> */}
-						<StyledTableCell align="center">
-							<select className=" p-2 w-28 text-center rounded-lg my-3" name="fk_id_estado" onChange={(e)=>handlerSaveHour({row, e})}>
-								<option selected="true" disabled="disabled">{row.hora}</option> 
-								{
-									hora.map((item, ind)=>(
-										<option value={ind}>{item}</option>
-									))
-								}	
-							</select>
-						</StyledTableCell>
-						
-						<StyledTableCell align="center"><Link href={`/citas/?${row.id_cita_paciente}`}>
-							<a>
-							<Button size="small">Ir a consulta</Button>
-							</a>
-							</Link> </StyledTableCell>
-					</StyledTableRow>
-				))} 
-				</TableBody>
-			</Table>
-		</TableContainer>
-	</div>
-	)
+		<div className="mx-2 sm:mx-10 mt-3">
+			<TableContainer className="h-96" component={Paper}>
+				<Table
+					className={classes.table}
+					size="small"
+					aria-label="a dense table"
+				>
+					<TableHead>
+						<TableRow>
+							<StyledTableCell align="left">Nombre</StyledTableCell>
+							<StyledTableCell align="center">Fecha</StyledTableCell>
+							<StyledTableCell align="center">Hora</StyledTableCell>
+							<StyledTableCell align="center">Accion</StyledTableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{data &&
+							data?.map((row, index) => (
+								<StyledTableRow key={index}>
+									<StyledTableCell align="left">
+										<p className=""> {`${row.nombre} ${row.apellidos}`}</p>
+									</StyledTableCell>
+									<StyledTableCell align="center">
+										{row.fecha_cita}
+									</StyledTableCell>
+									{/* <StyledTableCell align="center">{row.hora !== '' ? row.hora : 'Sin hora'}</StyledTableCell> */}
+									<StyledTableCell align="center">
+										<select
+											className=" p-2 w-28 text-center rounded-lg my-3"
+											name="fk_id_estado"
+											onChange={(e) => handlerSaveHour({ row, e })}
+										>
+											<option selected="true" disabled="disabled">
+												{row.hora}
+											</option>
+											{hora.map((item, ind) => (
+												<option value={ind}>{item}</option>
+											))}
+										</select>
+									</StyledTableCell>
+
+									<StyledTableCell align="center">
+										<Link href={`/citas/?${row.id_cita_paciente}`}>
+											<a>
+												<Button size="small">Ir a consulta</Button>
+											</a>
+										</Link>{' '}
+									</StyledTableCell>
+								</StyledTableRow>
+							))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</div>
+	);
 }
