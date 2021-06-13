5ba5f574cd959413facf7f1b1c4837f6a89e57ac
legarrod <luis.garcia@atlanticsoft.us>
2021-06-13 14:48:14 -0500

    copia de la rama donde se ha estado trabajando

--- next.config.js
+++ next.config.js
@@ -1,68 +1,67 @@
 module.exports = {
   env: {
     API_OBTENER_TODAS_LAS_CITAS:
-      "http://apioptica.recaudo.xyz/public/citas/api/citas",
+      'http://localhost/back-optica/public/citas/api/citas',
     API_OBTENER_LAS_CITAS_POR_ESTADO:
-      "http://apioptica.recaudo.xyz/public/citas/api/citasestado/",
+      'http://localhost/back-optica/public/citas/api/citasestado/',
     API_OBTENER_LAS_CITAS_POR_FECHA:
-      "http://apioptica.recaudo.xyz/public/citas/api/citasporfecha/",
+      'http://localhost/back-optica/public/citas/api/citasporfecha/',
     API_OBTENER_TODAS_LAS_CITAS_RESUMIDAS:
-      "http://apioptica.recaudo.xyz/public/citas/api/citas_cards",
+      'http://localhost/back-optica/public/citas/api/citas_cards',
     API_REGISTRAR_NUEVA_CITA:
-      "http://apioptica.recaudo.xyz/public/citas/api/citas/crear_cita",
+      'http://localhost/back-optica/public/citas/api/citas/crear_cita',
     API_ACTUALIZAR_CITA:
-      "http://apioptica.recaudo.xyz/public/citas/api/citas/update",
+      'http://localhost/back-optica/public/citas/api/citas/update',
     API_OBTENER_TODOS_LOS_PACIENTES:
-      "http://apioptica.recaudo.xyz/public/apipacientes/api/pacientes",
+      'http://localhost/back-optica/public/apipacientes/api/pacientes',
     API_BUSQUEDA_PACIENTES:
-      "http://apioptica.recaudo.xyz/public/apipacientes/api/pacientesList/",
+      'http://localhost/back-optica/public/apipacientes/api/pacientesList/',
     API_GUARDAR_PACIENTE:
-      "http://apioptica.recaudo.xyz/public/apipacientes/api/pacientes/nuevo",
+      'http://localhost/back-optica/public/apipacientes/api/pacientes/nuevo',
     API_CITA_POR_PACIENTE:
-      "http://apioptica.recaudo.xyz/public/citas/api/citaporpaciente",
+      'http://localhost/back-optica/public/citas/api/citaporpaciente',
     API_ELIMINAR_PACIENTE:
-      "http://apioptica.recaudo.xyz/public/apipacientes/api/pacientes/delete",
+      'http://localhost/back-optica/public/apipacientes/api/pacientes/delete',
     API_ACTUALIZAR_INFORMACION_DEL_PACIENTE:
-      "http://apioptica.recaudo.xyz/public/apipacientes/api/pacientes/update/",
+      'http://localhost/back-optica/public/apipacientes/api/pacientes/update/',
     API_CREAR_PRODUCTO:
-      "http://apioptica.recaudo.xyz/public/productos/api/productos/addProduct",
+      'http://localhost/back-optica/public/productos/api/productos/addProduct',
     API_OBTENER_TODOS_LOS_PRODUCTOS:
-      "http://apioptica.recaudo.xyz/public/productos/api/productos",
+      'http://localhost/back-optica/public/productos/api/productos',
     API_ACTUALIZAR_TODOS_LOS_PRODUCTOS:
-      "http://apioptica.recaudo.xyz/public/productos/api/productos/update",
+      'http://localhost/back-optica/public/productos/api/productos/update',
     API_ELIMINAR_PRODUCTO:
-      "http://apioptica.recaudo.xyz/public/productos/api/productos/delete/",
+      'http://localhost/back-optica/public/productos/api/productos/delete/',
     API_OBTENER_FACTURAS:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturacompleta",
+      'http://localhost/back-optica/public/contabilidad/api/facturacompleta',
     API_OBTENER_ABONOS_POR_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/abonosfactura/",
+      'http://localhost/back-optica/public/contabilidad/api/abonosfactura/',
     API_ELIMINAR_ABONO:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/abono/delete/",
+      'http://localhost/back-optica/public/contabilidad/api/abono/delete/',
     API_REGISTRAR_ABONO:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturas/crearabono",
+      'http://localhost/back-optica/public/contabilidad/api/facturas/crearabono',
     API_ELIMINAR_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturas/delete/",
+      'http://localhost/back-optica/public/contabilidad/api/facturas/delete/',
     API_CREAR_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturas/crearfactura",
+      'http://localhost/back-optica/public/contabilidad/api/facturas/crearfactura',
     API_OBTENER_ID_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturacodigo/",
+      'http://localhost/back-optica/public/contabilidad/api/facturacodigo/',
     API_CREAR_DETALLE_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturas/creardetallefactura",
+      'http://localhost/back-optica/public/contabilidad/api/facturas/creardetallefactura',
     API_OBTENER_FACTURA_PERSONA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturacedula/",
+      'http://localhost/back-optica/public/contabilidad/api/facturacedula/',
     API_OBTENER_DETALLER_POR_FACTURA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/detallefactura/",
+      'http://localhost/back-optica/public/contabilidad/api/detallefactura/',
     API_EXPORTAR:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/exportar",
-    API_LOGIN:
-      "http://apioptica.recaudo.xyz/public/usuarios/api/usuariologin",
+      'http://localhost/back-optica/public/contabilidad/api/exportar',
+    API_LOGIN: 'http://localhost/back-optica/public/usuarios/api/usuariologin',
     API_CITAS_POR_CEDULA:
-      "http://apioptica.recaudo.xyz/public/citas/api/citaporcedula/",
+      'http://localhost/back-optica/public/citas/api/citaporcedula/',
     API_ACTUALIZAR_ESTADO_CITA:
-      "http://apioptica.recaudo.xyz/public/contabilidad/api/facturas/actualizarestado/",
+      'http://localhost/back-optica/public/contabilidad/api/facturas/actualizarestado/',
     API_OBTENER_HORAS_CITAS_FECHA:
-      "http://apioptica.recaudo.xyz/public/citas/api/horascitasporfecha/",
+      'http://localhost/back-optica/public/citas/api/horascitasporfecha/',
     API_ACTUALIZAR_HORA_CITA:
-      "http://apioptica.recaudo.xyz/public/citas/api/horacita/update",
+      'http://localhost/back-optica/public/citas/api/horacita/update',
   },
 };
